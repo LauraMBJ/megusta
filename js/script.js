4c40c67b0e2c48d3d26c9a3ba82429492643f365
Laura Betancur <laura.ingfra@gmail.com>
2022-06-23 19:57:37 -0500

    primerrepositorio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var a =9;
+
+function like1(){
+    a++;
+    console.log(a);
+    var elemnt = document.querySelector("#l1");
+    elemnt.innerText = a;
+}
+
+var b =12;
+
+function like2(){
+    b++;
+    console.log(b);
+    var elemnt = document.querySelector("#l2");
+    elemnt.innerText = b;
+}
+
+var c =9;
+
+function like3(){
+    c++;
+    console.log(c);
+    var elemnt = document.querySelector("#l3");
+    elemnt.innerText = c;
+
+}
